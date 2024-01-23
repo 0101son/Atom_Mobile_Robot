777ae9e4cbcb1a7e6a3a9e5e88b71c59f010e059
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-01-23 13:23:08 +0900

    Update and rename motordriver.h to motor_driver.h

---
+++ opencr/motor_driver.h
@@ -0,0 +1,54 @@
+
+#ifndef MOTOR_DRIVER_H_
+#define MOTOR_DRIVER_H_
+
+#include <Arduino.h>
+
+#define TORQUE_ENABLE ControlTableItem::TORQUE_ENABLE
+
+enum MortorLocation{
+  LEFT = 0,
+  RIGHT,
+  MOTOR_NUM_MAX
+};
+
+enum VelocityType{
+  LINEAR = 0,
+  ANGULAR,
+  TYPE_NUM_MAX
+};
+
+
+class MotorDriver
+{
+ public:
+  MotorDriver();
+  ~MotorDriver();
+  
+  bool init(void);
+  void close(void);
+
+  bool is_connected();
+
+  bool set_torque(bool onoff);
+  bool get_torque();
+  
+  bool read_present_position(int32_t &left_value, int32_t &right_value);
+  bool read_present_velocity(int32_t &left_value, int32_t &right_value);
+  bool read_present_current(int16_t &left_value, int16_t &right_value);
+  bool read_profile_acceleration(uint32_t &left_value, uint32_t &right_value);
+  
+  bool write_velocity(int32_t left_value, int32_t right_value);
+  bool write_profile_acceleration(uint32_t left_value, uint32_t right_value);
+
+  bool control_motors(const float wheel_separation, float linear_value, float angular_value);
+
+  Dynamixel2Arduino& getDxl();
+  
+ private:
+  uint8_t left_wheel_id_;
+  uint8_t right_wheel_id_;
+  bool torque_;
+};
+
+#endif // TURTLEBOT3_MOTOR_DRIVER_H_
