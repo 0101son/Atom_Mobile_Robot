1c80139a6b2769ffd1999af5fa2de0ae7f880879
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-01-24 11:20:11 +0900

    Update serial_communication.h

--- Motor/serial_communication.h
+++ Motor/serial_communication.h
@@ -1,1 +1,16 @@
+#ifndef SERIAL_COMMUNICATION_H
+#define SERIAL_COMMUNICATION_H
 
+const int packetLength = 5;
+char receivedData[packetLength];
+
+const int motorPin = 9;
+const int minRPM = 0;
+const int maxRPM = 110;
+
+int parseDataToSpeed();
+void setMotorSpeed(int speed);
+void parseAndSetMotorSpeed();
+void serialEvent();
+
+#endif
