712ad53cce20adef81dcbd9b7afc7ccbf5a07d4b
ceuncha <155208795+ceuncha@users.noreply.github.com>
2024-01-28 21:05:27 +0900

    Update serial_communication.cpp

--- opencr/serial_communication.cpp
+++ opencr/serial_communication.cpp
@@ -1,1 +1,21 @@
+#include "serial_communication.h"
 
+RCStatus::RCStatus()
+: speedValues{0, 0, 0, 0}, angleValues{0, 0} 
+{}
+
+void RCStatus::setSpeedValue(SpeedField field, int value) {
+  speedValues[field] = value;
+}
+
+int RCStatus::getSpeedValue(SpeedField field) const {
+  return speedValues[field];
+}
+
+void RCStatus::setAngleValue(AngleField field, int value) {
+  angleValues[field] = value;
+}
+
+int RCStatus::getAngleValue(AngleField field) const {
+  return angleValues[field];
+}
