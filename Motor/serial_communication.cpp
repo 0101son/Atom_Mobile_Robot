372a14d0f248fa3367a84fdffb9cb6832471538f
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-01-30 16:53:35 +0900

    Update serial_communication.cpp

--- Motor/serial_communication.cpp
+++ Motor/serial_communication.cpp
@@ -39,3 +39,38 @@ void serialEvent() {
     }
   }
 }
+
+
+int i = 0;
+//String inputString = "";
+//int chValue[6] = { 0 };
+//
+//void setup() {
+//  // put your setup code here, to run once:
+//  Serial.begin(9600);
+//}
+//
+//void loop() {
+//  while (Serial.available() > 0) {
+//    char inChar = (char)Serial.read();
+//    inputString += inChar;
+//    if (inChar == ' ') {
+//      chValue[i] = inputString.toInt();
+//      inputString = "";
+//      i++;
+//    }
+//    else if (inChar == '\n') {
+//      chValue[i] = inputString.toInt();
+//      inputString = "";
+//      i = 0;
+//    }
+//  }
+//  for(int j=0; j<6; j++) {
+//    if (j != 5) {
+//      Serial.print(chValue[j]);
+//      Serial.print(" ");
+//    }
+//    else
+//      Serial.println(chValue[j]);
+//  }
+//}
