dfb6a0f2ec83adb8f49d204e019cbb877fbc915b
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-01-31 10:40:39 +0900

    Update controller.h

--- opencr/controller.h
+++ opencr/controller.h
@@ -4,14 +4,13 @@
 
 class Controller {
 public:
-  Controller(byte recvCH[], byte chNum = 6);
+  Controller(byte recvCH[]);
   void setValue(byte channel);
   unsigned long getValue(byte channel);
   void serialTransmission(byte channel);
 private:
-  byte chNum = 6;
-  unsigned long valueCH[chNum];
-  byte recvCH[chNum];
+  unsigned long valueCH[6];
+  byte recvCH[6];
 };
 
 #endif
