3d58fd8568f3612ee5edd2499a77077cc00e0aed
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-01-31 10:37:35 +0900

    Update controller.cpp

--- opencr/controller.cpp
+++ opencr/controller.cpp
@@ -1,19 +1,10 @@
 #include "controller.h"
 
-Controller::Controller(byte recvCH1, byte recvCH2, byte recvCH3,
-                      byte recvCH4, byte recvCH5, byte recvCH6) {
-  this->recvCH[0] = recvCH1;
-  this->recvCH[1] = recvCH2;
-  this->recvCH[2] = recvCH3;
-  this->recvCH[3] = recvCH4;
-  this->recvCH[4] = recvCH5;
-  this->recvCH[5] = recvCH6;
-  pinMode(recvCH1, INPUT);
-  pinMode(recvCH2, INPUT);
-  pinMode(recvCH3, INPUT);
-  pinMode(recvCH4, INPUT);
-  pinMode(recvCH5, INPUT);
-  pinMode(recvCH6, INPUT);
+Controller::Controller(byte recvCH[]) {
+  for (int i=0; i<6; i++) {
+    this->recvCH[i] = recvCH[i];
+    pinMode(recvCH[i], INPUT);
+  }
 }
 
 void Controller::setValue(byte channel) {
