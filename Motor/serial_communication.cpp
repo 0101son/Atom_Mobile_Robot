02665bf01400f7cada475fc07acd08dec3234aa5
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-02-02 04:37:47 +0900

    Update serial_communication.cpp

--- Motor/serial_communication.cpp
+++ Motor/serial_communication.cpp
@@ -1,76 +1,33 @@
-#include "serial_communication.h"
+#include "communication.h"
 #include <Arduino.h>
 
-int parseDataToSpeed() {
-  int speed = 0;
-  speed = atoi(receivedData);
-  if (speed < minRPM) {
-    speed = minRPM;
-  } else if (speed > maxRPM) {
-    speed = maxRPM;
-  }
-  return speed;
-}
-
-void setMotorSpeed(int speed) {
-  int motorOutput = map(speed, 0, maxRPM, 0, 255);
-  analogWrite(motorPin, motorOutput);
-}
-
-void parseAndSetMotorSpeed() {
-  int speed = parseDataToSpeed();
-  setMotorSpeed(speed);
+void parseData(char packet[]) {
+    char* token = strtok(packet, ",");  // 쉼표를 구분자로 사용하여 문자열을 자름
+    int count = 0;  // 데이터 개수를 세는 변수
+    int data[4];
+    while (token != NULL && count < data_count) {
+      data[count] = atoi(token);  // 문자열을 정수로 변환
+      token = strtok(NULL, " ");  // 다음 데이터를 자르기 위해 NULL을 전달하여 재사용
+      count++;
+    }
+    received_RPM = data[0];
+    received_steering = data[1];
+    received_motorDir = data[2];
+    received_steerMode = data[3];
 }
 
 void serialEvent() {
-  while (Serial.available()) {
-    char receivedChar = Serial.read();
-    if (receivedChar == 'S') {
-      memset(receivedData, 0, sizeof(receivedData));
-    }
-    else if (receivedChar == 'E') {
-      parseAndSetMotorSpeed();
-    }
-    else {
-      size_t dataLength = strlen(receivedData);
-      if (dataLength < packetLength - 1) {
-        receivedData[dataLength] = receivedChar;
-      }
-    }
+  if (Serial.available() >= packetLength) {  // 수신 버퍼에 패킷의 길이만큼 데이터가 있는지 확인
+    char packet[packetLength + 1];  // 패킷을 저장할 문자열 배열
+    Serial.readBytes(packet, packetLength);  // 패킷 데이터를 읽어서 배열에 저장
+    packet[packetLength] = '\0';  // 문자열 끝을 표시하기 위해 널 문자 추가
+    parseData(packet);
   }
 }
 
-
-int i = 0;
-//String inputString = "";
-//int chValue[6] = { 0 };
-//
-//void setup() {
-//  // put your setup code here, to run once:
-//  Serial.begin(9600);
-//}
-//
-//void loop() {
-//  while (Serial.available() > 0) {
-//    char inChar = (char)Serial.read();
-//    inputString += inChar;
-//    if (inChar == ' ') {
-//      chValue[i] = inputString.toInt();
-//      inputString = "";
-//      i++;
-//    }
-//    else if (inChar == '\n') {
-//      chValue[i] = inputString.toInt();
-//      inputString = "";
-//      i = 0;
-//    }
-//  }
-//  for(int j=0; j<6; j++) {
-//    if (j != 5) {
-//      Serial.print(chValue[j]);
-//      Serial.print(" ");
-//    }
-//    else
-//      Serial.println(chValue[j]);
-//  }
-//}
+void sendData() {
+  Serial.print(velocity[0]);
+  Serial.print(" ");
+  Serial.print(velocity[1]);
+  Serial.println();
+}
