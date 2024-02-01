f3a07413fa56626be066051b27fff820c3aa2f39
nahsu03 <154969612+nahsu03@users.noreply.github.com>
2024-02-01 16:54:01 +0900

    Add files via upload

---
+++ Motor/motor0130.ino
@@ -0,0 +1,119 @@
+#include <util/atomic.h>
+#include "pidcontrol.hpp"
+#include "serial_communication.hpp"
+
+// How many motors
+#define NMOTORS 2
+
+// Pins
+const int enca[2] = {2,3};
+const int encb[2] = {4,5};
+const int pwm[2] = {6,9};
+const int in1[2] = {7,12};
+const int in2[2] = {8,13};
+
+// Globals
+long prevT = 0;
+volatile int posi[2] = {0,0};
+int i = 0;
+String inputString = "";
+int chValue[6] = { 0 };
+
+// PID class instances
+PIDcontrol pid[NMOTORS];
+
+void setup() {
+  Serial.begin(9600);
+
+  for(int k = 0; k < NMOTORS; k++){
+    pinMode(enca[k],INPUT);
+    pinMode(encb[k],INPUT);
+    pinMode(pwm[k],OUTPUT);
+    pinMode(in1[k],OUTPUT);
+    pinMode(in2[k],OUTPUT);
+
+    pid[k].setParams(1,0,0,255);
+  }
+  
+  attachInterrupt(digitalPinToInterrupt(enca[0]),readEncoder<0>,RISING);
+  attachInterrupt(digitalPinToInterrupt(enca[1]),readEncoder<1>,RISING);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == ' ') {
+      chValue[i] = inputString.toInt();
+     inputString = "";
+     i++;
+   }
+   else if (inChar == '\n') {
+      chValue[i] = inputString.toInt();
+     inputString = "";
+     i = 0;
+   }
+  }
+  
+  // set target position
+  int target[NMOTORS];
+  target[0] = 990*sin(prevT/1e6);
+  target[1] = -990*sin(prevT/1e6);
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential misread
+  int pos[NMOTORS];
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+    for(int k = 0; k < NMOTORS; k++){
+      pos[k] = posi[k];
+    }
+  }
+  
+  // loop through the motors
+  for(int k = 0; k < NMOTORS; k++){
+    int pwr, dir;
+    // evaluate the control signal
+    pid[k].evalu(pos[k],target[k],deltaT,pwr,dir);
+    // signal the motor
+    setMotor(dir,pwr,pwm[k],in1[k],in2[k]);
+  }
+
+  for(int k = 0; k < NMOTORS; k++){
+    Serial.print(target[k]);
+    Serial.print(" ");
+    Serial.print(pos[k]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }  
+}
+
+template <int j>
+void readEncoder(){
+  int b = digitalRead(encb[j]);
+  if(b > 0){
+    posi[j]++;
+  }
+  else{
+    posi[j]--;
+  }
+}
