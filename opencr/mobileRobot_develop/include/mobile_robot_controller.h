dd193cc1fa606ea99c34486b5f36ed47bf1d20b8
0101son <0101son36@gmail.com>
2024-02-02 17:23:54 +0900

    controller class

---
+++ opencr/mobileRobot_develop/include/mobile_robot_controller.h
@@ -0,0 +1,24 @@
+#ifndef MOBILE_ROBOTBOT_CONTROLLER_H_
+#define MOBILE_ROBOTBOT_CONTROLLER_H_
+
+#include <IBusBM.h>
+
+class MobileRobotController
+{
+public:
+    MobileRobotController();
+    ~MobileRobotController();
+
+    bool init(float max_lin_vel, float max_ang);
+
+    void getRCdata(float *get_cmd_vel);
+
+private:
+    IBusBM IBus_;
+
+    // 계산을 위한 파라미터는 임의로 수정/추가
+    float max_lin_vel_;
+    float max_ang_;
+};
+
+#endif
