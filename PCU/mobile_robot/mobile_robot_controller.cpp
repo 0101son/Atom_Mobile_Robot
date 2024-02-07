2ebfae721fb124eaf417e8abfac875509d8cf1a4
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-02-07 14:43:33 +0900

    Update mobile_robot_controller.cpp

--- PCU/mobile_robot/mobile_robot_controller.cpp
+++ PCU/mobile_robot/mobile_robot_controller.cpp
@@ -1,5 +1,4 @@
 #include "mobile_robot_controller.h"
-#include <arduino.h>
 
 MobileRobotController::MobileRobotController() : iBus_(Serial1) {
 }
@@ -21,11 +20,10 @@ bool MobileRobotController::init(float max_lin_vel, float max_ang)
   return true;
 }
 
-void MobileRobotController::getRCdata(float get_cmd_vel[], int get_switch_val[])
+void MobileRobotController::getRCdata(byte get_cmd_vel[])
 {
 
-  float lin_x = 0.0, ang_z = 0.0;
-  int auto_ = 0, reverse = 0, drive_mod = 0, estop = 0;
+  byte lin_x = 0, ang_z = 0, auto_ = 0, reverse = 0, drive_mod = 0, estop = 0;
   
   // 여기에 입력값()을 lin_x(속력), ang_z(조향각)로 변환하는 계산 알고리즘 삽입
   // 계산을 위한 파라미터는 임의로 수정/추가
@@ -36,12 +34,12 @@ void MobileRobotController::getRCdata(float get_cmd_vel[], int get_switch_val[])
   
   iBus_.readRx();
 
-  lin_x = (float)(iBus_.readChannel(velChannel)-1000);
-  ang_z = (float)(iBus_.readChannel(steerChannel)-1000);
-  auto_ = (int)(iBus_.readChannel(manualChannel) / 1000 - 1);
-  reverse = (int)(iBus_.readChannel(reverseChannel) / 1000 - 1);
-  drive_mod = (int)(iBus_.readChannel(drivemodChannel) / 500 - 2);
-  estop = (int)(iBus_.readChannel(estopChannel) / 1000 - 1);
+  lin_x = (byte)(iBus_.readChannel(velChannel)-1000);
+  ang_z = (byte)(iBus_.readChannel(steerChannel)-1000);
+  auto_ = (byte)(iBus_.readChannel(manualChannel) / 1000 - 1);
+  reverse = (byte)(iBus_.readChannel(reverseChannel) / 1000 - 1);
+  drive_mod = (byte)(iBus_.readChannel(drivemodChannel) / 500 - 2);
+  estop = (byte)(iBus_.readChannel(estopChannel) / 1000 - 1);
   
   lin_x = constrain(lin_x, 0, 1000);
 
@@ -50,8 +48,8 @@ void MobileRobotController::getRCdata(float get_cmd_vel[], int get_switch_val[])
 
   get_cmd_vel[0] = lin_x;
   get_cmd_vel[1] = ang_z;
-  get_switch_val[0] = auto_;
-  get_switch_val[1] = reverse;
-  get_switch_val[2] = drive_mod;
-  get_switch_val[3] = estop;
+  get_cmd_vel[2] = auto_;
+  get_cmd_vel[3] = reverse;
+  get_cmd_vel[4] = drive_mod;
+  get_cmd_vel[5] = estop;
 }
