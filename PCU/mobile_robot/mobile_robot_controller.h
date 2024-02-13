2eaf8ee1fec4e2ed4d64fb385e9b75f82d3220c0
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-02-13 15:19:03 +0900

    Update mobile_robot_controller.h

--- PCU/mobile_robot/mobile_robot_controller.h
+++ PCU/mobile_robot/mobile_robot_controller.h
@@ -2,21 +2,26 @@
 #define MOBILE_ROBOTBOT_CONTROLLER_H_
 
 #include "ibus.h"
+#include <Arduino.h>
 
-# define velChannel 2
-# define SteerChannel 0
+#define velChannel 2
+#define steerChannel 0
+#define manualChannel 4
+#define reverseChannel 5
+#define drivemodChannel 6
+#define estopChannel 7
 
 class MobileRobotController
 {
-public:
+  public:
     MobileRobotController();
     ~MobileRobotController();
 
     bool init(float max_lin_vel, float max_ang);
 
-    void getRCdata(float *get_cmd_vel);
+    void getRCdata(byte get_cmd_vel[]);
 
-private:
+  private:
     IBus iBus_;
 
     // 계산을 위한 파라미터는 임의로 수정/추가
