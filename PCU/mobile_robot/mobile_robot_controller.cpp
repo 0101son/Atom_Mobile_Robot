d0631eb87f21a05fe8d0ab22c604961973088476
ngeusnim <128334097+ngeusnim@users.noreply.github.com>
2024-02-13 15:18:49 +0900

    Update mobile_robot_controller.cpp

--- PCU/mobile_robot/mobile_robot_controller.cpp
+++ PCU/mobile_robot/mobile_robot_controller.cpp
@@ -1,5 +1,4 @@
 #include "mobile_robot_controller.h"
-#include <arduino.h>
 
 MobileRobotController::MobileRobotController() : iBus_(Serial1) {
 }
@@ -10,21 +9,20 @@ MobileRobotController::~MobileRobotController()
 
 bool MobileRobotController::init(float max_lin_vel, float max_ang)
 {
-
   iBus_.init();
 
   max_lin_vel_ = max_lin_vel;
   max_ang_ = max_ang;
-  lin_vel_ratio_ = max_lin_vel_/500;
+  lin_vel_ratio_ = max_lin_vel_/1000;
   ang_ratio_ = max_ang_/1000;
 
   return true;
 }
 
-void MobileRobotController::getRCdata(float *get_cmd_vel)
+void MobileRobotController::getRCdata(byte get_cmd_vel[])
 {
 
-  float lin_x = 0.0, ang_z = 0.0;
+  uint16_t lin_x = 0, ang_z = 0;
   
   // 여기에 입력값()을 lin_x(속력), ang_z(조향각)로 변환하는 계산 알고리즘 삽입
   // 계산을 위한 파라미터는 임의로 수정/추가
@@ -34,15 +32,19 @@ void MobileRobotController::getRCdata(float *get_cmd_vel)
   // 예)
   
   iBus_.readRx();
-
-  lin_x = (float)(iBus_.readChannel(velChannel)-1500);
-  ang_z = (float)(iBus_.readChannel(SteerChannel)-1000);
-
-  lin_x = constrain(lin_x, 0, 500);
-
+  
+  lin_x = (iBus_.readChannel(velChannel)-1000);
+  ang_z = (iBus_.readChannel(steerChannel)-1000);
+  
+  lin_x = constrain(lin_x, 0, 1000);
+  
   lin_x = lin_x*lin_vel_ratio_;
   ang_z = ang_z*ang_ratio_;
-
-  get_cmd_vel[0] = lin_x;
-  get_cmd_vel[1] = ang_z;
+  
+  get_cmd_vel[0] = (byte)lin_x;
+  get_cmd_vel[1] = (byte)ang_z;
+  get_cmd_vel[2] = (byte)(iBus_.readChannel(manualChannel) / 1000 - 1);
+  get_cmd_vel[3] = (byte)(iBus_.readChannel(reverseChannel) / 1000 - 1);
+  get_cmd_vel[4] = (byte)(iBus_.readChannel(drivemodChannel) / 500 - 2);
+  get_cmd_vel[5] = (byte)(iBus_.readChannel(estopChannel) / 1000 - 1);
 }
